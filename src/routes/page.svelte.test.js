8b1807c9f1eb2bd656b40aca15e90f9c5899608c
wad2eq <mirekkedzierski@gmail.com>
2025-04-19 11:59:33 +0200

    add  test

--- src/routes/page.svelte.test.js
+++ src/routes/page.svelte.test.js
@@ -4,8 +4,19 @@ import { render, screen } from '@testing-library/svelte';
 import Page from './+page.svelte';
 
 describe('/+page.svelte', () => {
-	test('should render h1', () => {
-		render(Page);
-		expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
-	});
+    test('should render h1', () => {
+        const mockData = {
+            posts: {
+                data: {
+                    posts: [
+                        { title: 'Post 1', excerpt: 'Excerpt 1', featured_image: 'image1.jpg' },
+                        { title: 'Post 2', excerpt: 'Excerpt 2', featured_image: 'image2.jpg' }
+                    ]
+                }
+            }
+        };
+
+        render(Page, { data: mockData });
+        expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
+    });
 });

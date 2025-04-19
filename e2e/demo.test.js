8c82f79d8bcc3729ff802f71d0e2d0058d848006
wad2eq <mirekkedzierski@gmail.com>
2025-04-19 13:24:00 +0200

    Update Playwright dependency and refine home page test assertion

--- e2e/demo.test.js
+++ e2e/demo.test.js
@@ -2,5 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+	await expect(page.locator('h1')).toHaveText("Welcome to SvelteKit"); // Ensure h1 has some text
 });
+ 

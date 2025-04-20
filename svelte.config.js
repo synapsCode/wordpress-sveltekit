4e005cb494919fcc995e5bb8006e6836d8d8570e
wad2eq <mirekkedzierski@gmail.com>
2025-04-20 06:55:28 +0200

    Replace node adapter with Azure static web apps adapter in Svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
+import azure from '@sveltejs/adapter-azure-static-web-apps';
 
-const config = { kit: { adapter: adapter({
-    out: 'build',
-}) } };
+const config = { kit: { 
+    adapter: azure()
+ } };
 
 export default config;

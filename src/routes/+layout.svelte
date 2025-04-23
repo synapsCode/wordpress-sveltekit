b328fdd032f27fb8c862d55ed39bdbd2e8aafde1
wad2eq <mirekkedzierski@gmail.com>
2025-04-23 08:25:54 +0200

    add category post template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,34 @@
 <script lang="ts">
     import '$lib/styles.scss';
-let {children} = $props();
+    let {children} = $props();
 </script>
-<h2>mirek coś tam wie ale nie powie co</h2>
-{@render children()}
+
+<div class="pt-5 pb-5 has-background-black has-text-primary">
+    <header class="container level is-mobile warning has-background-black">
+        <h1 class="level-item level-left">Tyskfo tball</h1>
+        <nav class="menu">
+            <!-- TODO: jak zrobic aby menu byla elementem statycznym a nie dynamicznym S -->
+            <ul class="menu-list columns">
+                <li><a href="/category/bayern-munchen" class="has-text-primary">Bayern München</a></li>
+                <li><a href="/category/borussia-dortmund" class="has-text-primary">Borussia Dortmund</a></li>
+                <li><a href="/category/bayer-leverkusen" class="has-text-primary">Bayer Leverkusen</a></li>
+                <li><a href="/category/schalke-04" class="has-text-primary">Schalke 04</a></li>
+                <li><a href="/category/rb-leipzig" class="has-text-primary">RB Leipzig</a></li>
+                <li><a href="/category/hertha-berlin" class="has-text-primary">Hertha Berlin</a></li>
+                <li><a href="/category/wolfsburg" class="has-text-primary">Wolfsburg</a></li>
+                <li><a href="/category/eintracht-frankfurt" class="has-text-primary">Eintracht Frankfurt</a></li>
+            </ul>
+        </nav>
+    </header>
+</div>
+
+
+<main class="container pt-6">      
+    {@render children()}
+</main>
+
+<style lang="scss">
+    .menu-list li{
+        padding-left: var(--bulma-size-normal);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,18 +38,15 @@
 
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
 <div class="fixed-grid has-3-cols">
     <div class="grid is-gap-4 is-gap-1">
         {#each posts as item}
-        <div class="cell">
-            <img src={item.featured_image} alt={item.title} class="skeleton-block"/>
-            <h2>{item.title}</h2>
-            <p>{item.excerpt}</p>
-            <p>{@html item.excerpt}</p>
-        </div>
+            <div class="cell">
+                <img src={item.featured_image} alt={item.title} class="skeleton-block"/>
+                <h2>{item.title}</h2>
+                <p>{item.excerpt}</p>
+                <p>{@html item.excerpt}</p>
+            </div>
         {/each}
     </div>  
 </div>
